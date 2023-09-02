8ce720d5572fdb39fab5fb91353f35d844d7c352
DenisP <darvin2001@mail.ru>
2023-09-02 13:01:43 +0300

    Lesson

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -5,7 +5,9 @@ public class Lesson2 {
     public static void main(String[] args) {
 
         System.out.println("Тут был Ден");
-
+        System.out.println("Тут был Ден");
+        System.out.println("Тут был Ден");
+        System.out.println("Тут был Ден");
     }
 
 }
