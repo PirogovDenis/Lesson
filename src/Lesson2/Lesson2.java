3de71f7b08ff4691412fdad5106e0e8e238f2c5c
DenisP <darvin2001@mail.ru>
2023-09-02 13:32:03 +0300

    Lesson

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -1,13 +1,41 @@
 package Lesson2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lesson2 {
 
     public static void main(String[] args) {
+        System.out.println("""
+                ===========================
+                         Тут Тестит 
+                            Денис
+                ===========================""");
 
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
+        Scanner in = new Scanner(System.in); //Создание объекта, который может читать input из консоли
+        System.out.print("Введите 1е число: ");
+
+            int a = in.nextInt();            //Вызов метода объекта Scanner для чтения целого числа, которое пользователь вводит в консоль и сохранение ее в переменную.
+
+        System.out.print("Введите 2е число: ");
+
+            int b = in.nextInt();
+            int c = a + b;
+            System.out.println("Сумма чисел :" + c);
+
+            Random random = new Random();
+            int i = random.nextInt(c);
+
+            System.out.println("Рандом число, не выше суммы сложения : " + i);
+
+//Проверка Рандомного числа на четность
+
+            if (i % 2 == 0) {
+                System.out.println("Наше число \n чётное");
+
+            } else {
+                System.out.println("Наше число \n нечётное");
+
+            }
+        }
     }
-
-}
---
+++ src/Lesson4/Lesson4.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
