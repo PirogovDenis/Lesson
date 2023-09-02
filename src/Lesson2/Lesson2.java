9604e3463d1e43f89868bc3fe14a93f5854282da
DenisP <darvin2001@mail.ru>
2023-09-03 00:45:01 +0300

    Балавство с Гит привело к уничтожению Домашек (

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -4,10 +4,8 @@ public class Lesson2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
-        System.out.println("Тут был Ден");
+        System.out.println("ЗАШИБИСЬ ИМПОРТИРОВАЛ!!! Стерлось все что могло");
+
     }
 
 }
